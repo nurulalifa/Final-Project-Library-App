ae4505527baf0140795a876dac97f9044ba8a6d1
NURUL ALIFA PUTRI MUSLIM <190402051@student.umri.ac.id>
2023-05-22 13:46:01 +0700

    push index

--- deployData.js
+++ deployData.js
@@ -10,6 +10,6 @@ const studentName = 'Nurul Alifa Putri Muslim'; // TODO: replace this
 
 const studentId = 'FE5494991'; // TODO: replace this
 
-const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+const NetlifyDeployUrl = 'https://nurulalifa-fe5494991.netlify.app/'; // TODO: replace this
 
 module.exports = { studentName, studentId, NetlifyDeployUrl };
